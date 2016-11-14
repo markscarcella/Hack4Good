48fe4a359ca1f86110d3ead4df7cb931a8ca9762
Mark Scarcella <mark.scarcella@gmail.com>
2016-11-14 14:40:16 +1100

    adding client manager and drum script

---
+++ Hack4GoodUnity/Assets/Scripts/Client/ClientManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClientManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Connect()
+	{
+		// connect to server with instrument type
+	}
+
+	public void PlayNoteVelocity(float note, float velocity)
+	{
+		// send note and velocity to server
+	}
+}
---
+++ Hack4GoodUnity/Assets/Scripts/Client/Drum.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drum : MonoBehaviour {
+
+	float accelerometerUpdateInterval = 1.0f / 60.0f;
+	// The greater the value of LowPassKernelWidthInSeconds, the slower the filtered value will converge towards current input sample (and vice versa).
+	float lowPassKernelWidthInSeconds = 1.0f;
+	// This next parameter is initialized to 2.0 per Apple's recommendation, or at least according to Brady! ;)
+	public float shakeDetectionThreshold = 1.5f;
+
+	private float lowPassFilterFactor;
+	private Vector3 lowPassValue;
+	private Vector3 acceleration;
+	private Vector3 deltaAcceleration;
+
+	ClientManager clientManager;
+
+	void Start()
+	{
+		clientManager = GameObject.Find("ClientManager");
+
+		lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+		lowPassValue = Vector3.zero;
+		shakeDetectionThreshold *= shakeDetectionThreshold;
+		lowPassValue = Input.acceleration;
+	}
+
+	void Update()
+	{
+		acceleration = Input.acceleration;
+		lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
+		deltaAcceleration = acceleration - lowPassValue;
+		if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold)
+		{
+			clientManager.PlayNoteVelocity(0.0f, deltaAcceleration.sqrMagnitude);
+		}
+	}
+}
