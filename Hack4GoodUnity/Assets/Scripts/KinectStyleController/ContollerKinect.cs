fe1ac015d13bfdfecb27239dbf4602c5941daf1a
DESKTOP-7LG739E\Destined <j.oliver@destined.com>
2016-11-15 08:41:18 +1100

    adding kinect

---
+++ Hack4GoodUnity/Assets/Scripts/KinectStyleController/ContollerKinect.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContollerKinect : MonoBehaviour {
+
+
+	public Transform leftHand; 
+	public Transform rightHand;
+	public Transform head; 
+	public Transform hip;
+	public Transform leftFoot;
+	public Transform rightFoot;
+
+	public GameObject[] notes;
+
+	public GameObject noteRightHand;
+	public GameObject noteLeftHand;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (leftHand != null)
+		{
+			CheckLeftHand ();
+
+		} 
+		else if(noteLeftHand != null)
+		{
+			noteLeftHand.GetComponent<NoteFloatOffPlay> ().StartDeath ();
+			noteLeftHand = null;
+		}
+		if (rightHand != null)
+		{
+			CheckRightHand ();
+
+		} 
+		else if(noteRightHand != null)
+		{
+			noteRightHand.GetComponent<NoteFloatOffPlay> ().StartDeath ();
+			noteRightHand = null;
+		}
+
+	}
+
+	void CheckLeftHand()
+	{
+		if (leftHand.position.y < hip.position.y && noteLeftHand == null)
+		{
+			noteLeftHand = Instantiate (notes [Random.Range(0,2)], leftHand.position, Quaternion.identity) as GameObject;
+		}
+
+		if (leftHand.position.y > head.position.y && noteLeftHand != null)
+		{
+			noteLeftHand.GetComponent<NoteFloatOffPlay> ().StartDeath ();
+			noteLeftHand = null;
+		}
+
+		if (noteLeftHand != null)
+		{
+			noteLeftHand.transform.position = leftHand.position;
+		}
+	}
+
+	void CheckRightHand()
+	{
+		if (rightHand.position.y < hip.position.y && noteRightHand == null)
+		{
+			noteRightHand = Instantiate (notes [Random.Range(0,2)], rightHand.position, Quaternion.identity) as GameObject;
+
+		}
+
+		if (rightHand.position.y > head.position.y && noteRightHand != null)
+		{
+			noteRightHand.GetComponent<NoteFloatOffPlay> ().StartDeath ();
+			noteRightHand = null;
+		}
+
+		if (noteRightHand != null)
+		{
+			noteRightHand.transform.position = rightHand.position;
+		}
+	}
+}
---
+++ Hack4GoodUnity/Assets/Scripts/KinectStyleController/NoteFloatOffPlay.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteFloatOffPlay : MonoBehaviour {
+
+	private float timeToDestroy = 3.0f;
+
+	private bool isFlying = false;
+
+	private float amtLeft = 0.0f;
+	private float amtRight = 0.0f;
+	// Use this for initialization
+	void Start () 
+	{
+		amtLeft = Random.Range (0.0f, 1.0f);
+		amtRight = Random.Range (0.0f, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isFlying)
+		{
+			timeToDestroy -= Time.deltaTime;
+			transform.position += new Vector3 (amtRight * Time.deltaTime * 1.0f, Time.deltaTime* 1.0f, amtLeft * Time.deltaTime * 1.0f);
+			if (timeToDestroy < 0)
+			{
+				Destroy (this);
+			}
+		}
+	}
+
+	public void StartDeath()
+	{
+		isFlying = true;
+		Debug.Log ("Starting Death");
+	}
+}
