cda96aec80e370574a5d82fc06ef1c7727dfe0ca
Mark Scarcella <mark.scarcella@gmail.com>
2016-11-14 20:29:38 +1100

    fixed trail lengths

--- Hack4GoodUnity/Assets/Scripts/Client/SwipePiano/TouchTrail.cs
+++ Hack4GoodUnity/Assets/Scripts/Client/SwipePiano/TouchTrail.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class TouchTrail : MonoBehaviour {
 
+	BeatCounter beatCounter;
+
 	public int touchIdx;
 	public TrailRenderer trailRenderer;
 
 	// Use this for initialization
 	void Start () {
 		trailRenderer = GetComponent<TrailRenderer>();
+		beatCounter = GameObject.Find("BeatCounter").GetComponent<BeatCounter>();
+		trailRenderer.time = (float)beatCounter.BarLength;
 	}
 	
 	// Update is called once per frame
@@ -17,13 +21,13 @@ public class TouchTrail : MonoBehaviour {
 		{
 			Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.touches[touchIdx].position);
 			transform.position = new Vector3(touchPos.x, touchPos.y, 0.0f);
-			trailRenderer.startWidth = Camera.main.WorldToViewportPoint(transform.position).y;
-			trailRenderer.material.color = Color.HSVToRGB(Camera.main.WorldToViewportPoint(transform.position).x, 0.5f, 1.0f);
+			//trailRenderer.startWidth = Camera.main.WorldToViewportPoint(transform.position).y;
+			trailRenderer.material.color = Color.HSVToRGB(Camera.main.WorldToViewportPoint(transform.position).y, 0.5f, 1.0f);
 		}	
 		else
 		{
 			touchIdx = 99;
-			Destroy(gameObject, 2.0f);
+			//Destroy(gameObject, .0f);
 		}
 	}
 }
